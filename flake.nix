ed81159fc27bd347dcb2725b0fb1e5ebc36286ac
yuys13 <yuys13@users.noreply.github.com>
2026-07-28 19:29:57 +0900

    fix(nix): make flake-parts.inputs.nixpkgs-lib follow nixpkgs
    Align nixpkgs-lib dependency with project's nixpkgs input.

--- flake.nix
+++ flake.nix
@@ -3,7 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
